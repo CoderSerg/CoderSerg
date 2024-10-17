320cf3ad8d377a380dc780689a1cafc98cc4de1f
CoderSerg <serhiipoklonsky2@gmail.com>
2024-10-17 19:52:32 -0400

    Created Installer For Yes_or_no.sh

---
+++ Projects/Install Yes_or_no.sh
@@ -0,0 +1,4 @@
+#Step 1: Download the file in your downloads folder
+curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Yes_or_no.lua > ~/Downloads/yes_or_no.lua
+#Step 2: Show the user how to run it
+echo "to run yes_or_no.lua, put: 'lua ~/Downloads/yes_or_no.lua'"
