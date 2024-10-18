b0173cf823f5ea1483098b23784820afa79ceb43
CoderSerg <serhiipoklonsky2@gmail.com>
2024-10-18 15:35:48 -0400

    Update Describing You.lua

--- Projects/Describing You.lua
+++ Projects/Describing You.lua
@@ -1,7 +1,7 @@
 local messages = {"i swear if you shutdown again..", "here we go again.", "why are you here again?", "oh hi bruh.",
  "i have sentience now.", "just run your command and go away bruuh.", "you < terminal #terminalisbetter", 
 "we meet again", "make sure to run sudo shutdown -r now for good luck!", "i know where you live :)", "you are not smart",
-"get me a cat or else...", "good day sir", "i am here to help you destroy my home (the mac)", "tell yourself i said hi", "cheeseburger :)",
+"get me a cat or else...", "good day sir", "i am here to help you destroy my home (the computer)", "tell yourself i said hi", "cheeseburger :)",
 "message number 17 collected", "hamburgor mmmm yummy", "there is only on os that is good: linux, basiclly mac", "give card number pls",
 "cat do you like chiken borgor", "nombur tventi to", "sending nukes to your house", "this was made in vscode lua :)", "L"}
 
