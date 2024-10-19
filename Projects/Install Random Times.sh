9b446d2e199ec645c9d8c3e45459d124ab528ded
Serg <serhiipoklonsky2@gmail.com>
2024-10-18 20:04:52 -0400

    yippe

---
+++ Projects/Install Random Times.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/random_times.lua > ~/Downloads/random_times.lua
+clear
+echo "to run the file put: 'lua ~/Downloads/random_times.lua'"
