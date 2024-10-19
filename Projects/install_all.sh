8a0a1a1880e6b4f9b3835889bbee32e0e4d3bd27
Serg <serhiipoklonsky2@gmail.com>
2024-10-18 20:07:10 -0400

    Up to date with projects (4)

--- Projects/install_all.sh
+++ Projects/install_all.sh
@@ -12,10 +12,15 @@ cd CoderSerg
 curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Describing%20You.lua" > dy.lua
 curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Rating%20You.lua" > ry.lua
 curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Yes_or_no.lua" > yon.lua
+curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/random_times.lua" > rt.lua
 clear
 echo "Lua files successfully installed into ~/Downloads/CoderSerg..."
 sleep '0.4'
 echo "Files are being verified please wait..."
 sleep '2'
 clear
-echo "You are ready! Just one last step. Please paste this at the bottom of your .zshrc file: 'dy() {lua ~/Downloads/CoderSerg/dy.lua} ry() {lua ~/Downloads/CoderSerg/ry.lua} yon() {lua ~/Downloads/CoderSerg/yon.lua}"
+echo "You are ready! Just one last step. Please paste this at the bottom of your .zshrc file: 
+dy() {lua ~/Downloads/CoderSerg/dy.lua} 
+ry() {lua ~/Downloads/CoderSerg/ry.lua} 
+yon() {lua ~/Downloads/CoderSerg/yon.lua} 
+rt() {lua ~/Downloads/CoderSerg/rt.lua"
