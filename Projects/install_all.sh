5082f78a1984f1ba5c3b8d76f1c03d45ea45ae5e
Serg <serhiipoklonsky2@gmail.com>
2024-10-18 20:44:56 -0400

    Release v1.0.2 Beta

--- Projects/install_all.sh
+++ Projects/install_all.sh
@@ -8,20 +8,20 @@ sleep '0.3'
 echo "Installing in 5s Press Control+C to cancel..."
 sleep '5'
 cd ~
-mkdir CoderSerg
-cd CoderSerg
+mkdir CoderSergProjects
+cd CoderSergProjects
 curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Describing%20You.lua" > dy.lua
 curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Rating%20You.lua" > ry.lua
 curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Yes_or_no.lua" > yon.lua
 curl "https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/random_times.lua" > rt.lua
 clear
-echo "Lua files successfully installed into ~/CoderSerg..."
+echo "Lua files successfully installed into ~/CoderSergProjects..."
 sleep '0.4'
 echo "Files are being verified please wait..."
 sleep '2'
 clear
 echo "You are ready! Just one last step. Please paste this at the bottom of your .zshrc file: 
-dy() {lua ~/CoderSerg/dy.lua} 
-ry() {lua ~/CoderSerg/ry.lua} 
-yon() {lua ~/CoderSerg/yon.lua} 
-rt() {lua ~/CoderSerg/rt.lua}"
+dy() {lua ~/CoderSergProjects/dy.lua} 
+ry() {lua ~/CoderSergProjects/ry.lua} 
+yon() {lua ~/CoderSergProjects/yon.lua} 
+rt() {lua ~/CoderSergProjects/rt.lua}"
