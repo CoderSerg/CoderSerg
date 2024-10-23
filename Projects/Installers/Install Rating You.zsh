4b6348e508612700810b4c19e4eaae354c80b6c7
Serg <serhiipoklonsky2@gmail.com>
2024-10-23 07:46:46 -0400

    moved

--- Projects/Installers/Install Rating You.zsh
+++ Projects/Installers/Install Rating You.zsh
@@ -1,5 +1,5 @@
 #Step 1: Download the file in your downloads folder
-curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Rating%20You.lua > ~/Downloads/rating_you.lua
+curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Code/Rating%20You.lua > ~/Downloads/rating_you.lua
 #Step 2: Show the user how to run it
 clear
 echo "to run rating_you.lua, put: 'lua ~/Downloads/rating_you.lua'"
