6ea557117090ad27c613b7f73710b3f33613c163
Serg <serhiipoklonsky2@gmail.com>
2024-10-23 07:40:34 -0400

    moved

--- Projects/Installers/Install Random Times.zsh
+++ Projects/Installers/Install Random Times.zsh
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/random_times.lua > ~/Downloads/random_times.lua
+curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Code/random_times.lua > ~/Downloads/random_times.lua
 clear
 echo "to run the file put: 'lua ~/Downloads/random_times.lua'"
