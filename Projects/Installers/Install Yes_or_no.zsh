86b2fefc65a300070dcdb40e8824b3c5b86c9b56
Serg <serhiipoklonsky2@gmail.com>
2024-10-23 07:47:03 -0400

    Update Install Yes_or_no.zsh

--- Projects/Installers/Install Yes_or_no.zsh
+++ Projects/Installers/Install Yes_or_no.zsh
@@ -1,5 +1,5 @@
 #Step 1: Download the file in your downloads folder
-curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Yes_or_no.lua > ~/Downloads/yes_or_no.lua
+curl https://raw.githubusercontent.com/CoderSerg/CoderSerg/refs/heads/main/Projects/Code/Yes_or_no.lua > ~/Downloads/yes_or_no.lua
 #Step 2: Show the user how to run it
 clear
 echo "to run yes_or_no.lua, put: 'lua ~/Downloads/yes_or_no.lua'"
